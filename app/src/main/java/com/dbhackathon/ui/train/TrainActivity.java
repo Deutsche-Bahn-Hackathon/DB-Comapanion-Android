74439330e67b746fc241431b3003a3b03ede260c
Alex <alex.lardschneider@gmail.com>
2016-12-17 04:42:08 +0100

    Improved train layout

--- app/src/main/java/com/dbhackathon/ui/train/TrainActivity.java
+++ app/src/main/java/com/dbhackathon/ui/train/TrainActivity.java
@@ -2,12 +2,24 @@ package com.dbhackathon.ui.train;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.dbhackathon.R;
 import com.dbhackathon.data.model.Train;
 import com.dbhackathon.ui.BaseActivity;
 
-public class TrainActivity extends BaseActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class TrainActivity extends BaseActivity implements View.OnClickListener {
+
+    @BindView(R.id.train_action_alarm) RelativeLayout mActionAlarm;
+    @BindView(R.id.train_action_bar) RelativeLayout mActionBars;
+    @BindView(R.id.train_action_toilets) RelativeLayout mActionToilets;
+    @BindView(R.id.train_action_attractions) RelativeLayout mActionAttractions;
+    @BindView(R.id.train_action_survey) RelativeLayout mActionSurveys;
+    @BindView(R.id.train_action_statistics) RelativeLayout mActionStatistics;
 
     private Train mTrain;
 
@@ -16,6 +28,7 @@ public class TrainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_train);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
 
@@ -26,10 +39,24 @@ public class TrainActivity extends BaseActivity {
         }
 
         mTrain = intent.getParcelableExtra(Config.EXTRA_TRAIN);*/
+
+        mActionAlarm.setOnClickListener(this);
+        mActionBars.setOnClickListener(this);
+        mActionToilets.setOnClickListener(this);
+        mActionAttractions.setOnClickListener(this);
+        mActionSurveys.setOnClickListener(this);
+        mActionStatistics.setOnClickListener(this);
     }
 
     @Override
     protected int getNavItem() {
         return NAVDRAWER_ITEM_TRAIN;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+        }
     }
 }
